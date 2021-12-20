7bacbc99e19754914031d6ff7702c1699ca5829e
francesca <fradeluca85@gmail.com>
2021-12-20 18:41:43 +0100

    ricerca film v-model

--- src/components/commons/SearchBar.vue
+++ src/components/commons/SearchBar.vue
@@ -1,7 +1,7 @@
 <template>
     <section>
         <div class="search-bar">
-            <input type="text" placeholder="cerca film">
+            <input type="text" placeholder="cerca movie" v-model="searchMovie">
             <button @click="ricerca">cerca</button>
         </div>
     </section>
@@ -11,18 +11,25 @@
 import axios from 'axios'
 export default {
         name: 'searchBar',
-        
+        data() {
+            return {
+                movie: [],
+                searchMovie: ''
+            }
+        },
         methods: {
             ricerca() {
                 axios.get('https://api.themoviedb.org/3/search/movie', {
                         params: {
                             api_key: 'd709e60f4da8bcd79a37874605d0272c',
-                            query: '',
+                            query: this.searchMovie,
                             language: 'it-IT'
                         }
-                    })                    
-                    .then(function (response) {
-                        console.log(response);
+                    })
+                    .then((response) => {
+                        console.log(response.data.results);
+                        this.movie = response.data.results
+                        console.log(this.movie);
                     })
                     .catch(function (error) {
                         console.log(error);
