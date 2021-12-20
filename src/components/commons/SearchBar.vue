39395753a00b8640248eea979b63cfaa3dd4d1d2
francesca <fradeluca85@gmail.com>
2021-12-20 17:37:20 +0100

    search bar

---
+++ src/components/commons/SearchBar.vue
@@ -0,0 +1,17 @@
+<template>
+    <section>
+        <div class="search-bar">
+            <input type="text" placeholder="cerca film">
+            <button>cerca</button>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'SearchBar'
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/components/macro/Header.vue
@@ -0,0 +1,19 @@
+<template>
+    <header>
+        <SearchBar/>
+    </header>
+</template>
+
+<script>
+import SearchBar from '../commons/SearchBar.vue';
+export default {
+    name: 'Header',
+    components: {
+        SearchBar
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
