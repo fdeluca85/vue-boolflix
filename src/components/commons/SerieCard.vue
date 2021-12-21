03319112eb5421d4fd493ae4b986bc1d489b8a4c
francesca <fradeluca85@gmail.com>
2021-12-21 18:20:23 +0100

    milestone 2 - bandiera lingua

--- src/components/commons/SerieCard.vue
+++ src/components/commons/SerieCard.vue
@@ -4,6 +4,8 @@
             <li>{{info.name}}</li>
             <li>{{info.original_name}}</li>
             <li>{{info.vote_average}}</li>
+            <li>Lingua originale: {{lingua()}}</li>
+
         </ul>
         
     </div>
@@ -15,6 +17,23 @@ export default {
     name: 'SerieCard',
     props: {
         info: Object
+    },
+    methods:{
+        lingua(){
+            if (this.info.original_language == 'it') {
+                    return '\uD83C\uDDEE\uD83C\uDDF9'
+                } else if (this.info.original_language == 'en') {
+                    return '\uD83C\uDDFA\uD83C\uDDF8'
+                } else if (this.info.original_language == 'fr') {
+                    return '\uD83C\uDDE8\uD83C\uDDF5'
+                } else if (this.info.original_language == 'es') {
+                    return '\uD83C\uDDEA\uD83C\uDDF8'
+                } else if (this.info.original_language == 'ja') {
+                    return '\uD83C\uDDEF\uD83C\uDDF5'
+                } else {
+                    return this.info.original_language
+                }
+        }
     }
 }
 </script>
